008efc26b8f9780d9f7f5d045c97f878a1c6d8d7
mahi-A1232 <167527970+mahi-A1232@users.noreply.github.com>
2024-05-08 13:23:19 +0530

    Add files via upload
    hi

---
+++ formdb.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>form</title>
+</head>
+<body>
+    <form action="connect.php" method="post">
+       name : <input type="text" name = "name">  
+       lang : <input type="radio" name = "lang" value="Telugu">Telugu  
+        <input type="radio" name="lang" value = "Hindi">Hindi
+        <input type="submit">
+    </form>
+</body>
+</html>
